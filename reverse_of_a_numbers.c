71cd60abc4265669ca3bcc4d0b21f621144d4aa9
gangamaheswari <119841170+gangamaheswari@users.noreply.github.com>
2023-01-25 11:39:43 +0530

    #include<stdio.h> int main() {     int i,n,r,q,s=0;     scanf("%d",&n);     while(n!=0)     {         r=n%10;         n=n/10;         s=s*10+r;     }     printf("%d",s);
    }

---
+++ reverse_of_a_numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,r,q,s=0;
+    scanf("%d",&n);
+    while(n!=0)
+    {
+        r=n%10;
+        n=n/10;
+        s=s*10+r;
+    }
+    printf("%d",s);
+    
+}
